915aa6be7601adba725be596cf52142613f3c5f3
Brandon Arriaga <brandonzarriaga@gmail.com>
2024-04-26 16:53:51 -0700

    Added new possible intents and configured them into the findGeneralIntent method to broaden for user input

--- src/main/java/dataManipulation/ChatNLPProcessing.java
+++ src/main/java/dataManipulation/ChatNLPProcessing.java
@@ -3,9 +3,12 @@ package dataManipulation;
 import dataContainers.DatedSyllabusEntities;
 import dataContainers.Syllabus;
 import dataContainers.SyllabusEntities;
+import javafx.geometry.Pos;
 import ui.StudentRawInfo;
+import edu.stanford.nlp.simple.*;
 
 import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ChatNLPProcessing {
@@ -37,12 +40,36 @@ public class ChatNLPProcessing {
     private List<String> findProfessors (String input) {
         return new LinkedList<>();
     }
+
+
     public PossibleIntents findGeneralIntent(String input) {
-        Pattern datesPattern = Pattern.compile ("date|schedule");
-        if (datesPattern.matcher(input.toLowerCase()).find())
-            return PossibleIntents.SEE_DATES;
+        Pattern datesPattern = Pattern.compile ("date|schedule|test|office hours|textbook|professor|location|grading policy|syllabus overview");
+        Matcher matcher = datesPattern.matcher(input.toLowerCase());
+
+        if (matcher.find()) {
+            String matchedKeyword = matcher.group();
+            if (matchedKeyword.contains("date") || matchedKeyword.contains("schedule")) {
+                return PossibleIntents.SEE_DATES;
+            } else if (matchedKeyword.contains("test")) {
+                return PossibleIntents.GENERATE_TEST_SCHEDULE;
+            } else if (matchedKeyword.contains("office hours")) {
+                return PossibleIntents.OFFICE_HOUR_TIMES;
+            } else if (matchedKeyword.contains("textbook")) {
+                return PossibleIntents.RECOMMEND_TEXTBOOK;
+            } else if (matchedKeyword.contains("professor")) {
+                return PossibleIntents.GET_PROFESSOR_INFO;
+            } else if (matchedKeyword.contains("location")) {
+                return PossibleIntents.GET_LOCATION;
+            } else if (matchedKeyword.contains("grading policy")) {
+                return PossibleIntents.GET_GRADING_POLICY;
+            } else if (matchedKeyword.contains("syllabus overview")) {
+                return PossibleIntents.SYLLABUS_OVERVIEW;
+            }
+        }
         return PossibleIntents.UNKNOWN;
     }
+
+
     public void addSyllabus (String profName, Syllabus syllabus) {
         syllabi.put (profName, syllabus);
     }
