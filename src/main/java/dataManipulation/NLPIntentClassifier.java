c38bb25e24e3aa8c4ed058965f19215e6e7a6a3d
Vaishnavi <vkouru07@gmail.com>
2024-04-28 19:25:57 -0700

    nlp-intent-classifier: working J48 model

--- src/main/java/dataManipulation/NLPIntentClassifier.java
+++ src/main/java/dataManipulation/NLPIntentClassifier.java
@@ -1,26 +1,33 @@
 package dataManipulation;
 
 import weka.classifiers.Classifier;
-import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.meta.FilteredClassifier;
+import weka.classifiers.trees.J48;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.StringToWordVector;
 
 import java.io.*;
 
 public class NLPIntentClassifier {
 
     private static final String TRAINING_FILE = "/trainingdata/chattraining.arff";
-    private static final String MODEL_FILE = "/trainingdata/message_classifier.model";
+    private static final String MODEL_FILE = "intent_model.model";
 
     public static void main(String[] args) {
         try {
             // Step 1: Load training data
             Instances trainingData = loadTrainingData(TRAINING_FILE);
 
-            // Step 2: Train the classifier
-            Classifier classifier = trainClassifier(trainingData);
+            // Step 2: Apply StringToWordVector filter
+            Instances filteredData = applyStringToWordVectorFilter(trainingData);
 
-            // Step 3: Save the trained model
+            // Step 3: Train the classifier
+            Classifier classifier = trainClassifier(filteredData);
+
+            // Step 4: Save the trained model
             saveModel(classifier, MODEL_FILE);
 
             System.out.println("Model trained and saved successfully.");
@@ -37,8 +44,15 @@ public class NLPIntentClassifier {
         return data;
     }
 
+
+    private static Instances applyStringToWordVectorFilter(Instances data) throws Exception {
+        StringToWordVector filter = new StringToWordVector();
+        filter.setInputFormat(data);
+        return Filter.useFilter(data, filter);
+    }
+
     private static Classifier trainClassifier(Instances trainingData) throws Exception {
-        Classifier classifier = new NaiveBayes();
+        Classifier classifier = new J48(); // Use J48 decision tree classifier
         classifier.buildClassifier(trainingData);
         return classifier;
     }
@@ -49,3 +63,4 @@ public class NLPIntentClassifier {
         oos.close();
     }
 }
+
