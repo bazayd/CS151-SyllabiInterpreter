17e834edc8a211020093ce2152844ff78cbde4f1
Ace Lavilla <aceguillerlavilla@gmail.com>
2024-04-30 20:32:27 -0700

    fixed up some things

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,12 +2,14 @@ module com.app.cs151synter {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.kordamp.bootstrapfx.core;
+    //requires org.kordamp.bootstrapfx.core;
     requires stanford.corenlp;
     requires java.logging;
     requires convertapi;
     requires pdfbox;
 
-    opens com.app.cs151synter to javafx.fxml;
+    opens com.app.cs151synter to javafx.fxml, javafx.graphics;
     exports com.app.cs151synter;
+    opens com.app.cs151synter.ui to javafx.graphics, javafx.fxml;
+    exports com.app.cs151synter.ui;
 }
